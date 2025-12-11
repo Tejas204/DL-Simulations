76200b9b8e21d819d96c5856685f297870ce1f64
Tejas204 <tejasdhopavkar2000@gmail.com>
2025-12-11 17:29:46 +0100

    Class to select subset

--- Data_Subset_Selection/data_subset.py
+++ Data_Subset_Selection/data_subset.py
@@ -4,15 +4,46 @@ import matplotlib.pyplot as plt
 
 # Create Class for subset selection
 class DataSubset():
+    """
+    Class: DataSubset
+    Used to select a subset from a given dataset
+    """
+
     def __init__(self):
+        """
+        Initialize the class DataSubset
+        
+        :param self: Defines constants
+        """
         self.timestamps = np.array([i for i in range(100)])
 
     def __getitem__(self, index):
+        """
+        Return data at specific timestamp
+        
+        :param self: Class specific parameters
+        :param index: to fetch the timestamp at given index
+        """
         return self.timestamps[index]
     
     def remove_time_stamps(self, start, end):
-        pass
+        """
+        Remove data at timestamps from start index to end index.
+        Return both subsets
+        
+        :param self: Class specific parameters
+        :param start: start index of removal
+        :param end: end index of removal
+        """
+        removed_timestamps = self.timestamps[start: end]
 
-# Create a random number array and sort it
-# Reason: Timestamps go from 0 to 100 --> Ascending order
-# print(f"Timestamps are: {timestamps}")
+        if start == 0:
+            selected_timestamps = self.timestamps[end: ]
+        else:
+            selected_timestamps = np.append(self.timestamps[: start], self.timestamps[end: ])
+
+        return selected_timestamps, removed_timestamps
+
+
+subset = DataSubset()
+
